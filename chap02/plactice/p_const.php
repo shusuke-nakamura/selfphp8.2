99eeb2be239989776f79934021faddf6b69e9144
Shusuke Nakamura <shusuke.pana@gmail.com>
2022-12-31 00:34:23 +0900

    定数の学習完了

---
+++ chap02/plactice/p_const.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>定数</title>
+</head>
+<body>
+実行中のファイル名：<?= __FILE__ ?><br />
+実行中の行番号：<?= __LINE__ ?>
+</body>
+</html>
