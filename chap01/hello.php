b124d14f8766d0484cad0bc928aa6b9eca0546af
Shusuke Nakamura <shusuke.pana@gmail.com>
2022-12-30 22:13:05 +0900

    chap01の学習完了

---
+++ chap01/hello.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHPの基本</title>
+</head>
+<body>
+<?php
+print 'こんにちは、世界！<br />';
+print 'こんにちは、皆さん!';
+?>
+</body>
+</html>
