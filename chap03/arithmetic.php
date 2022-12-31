08d1c99e722d356631417b6d05e0d4cf944b1cc9
Shusuke Nakamura <shusuke.pana@gmail.com>
2022-12-31 21:41:10 +0900

    3.1.4. 浮動小数点の演算に注意まで学習完了

---
+++ chap03/arithmetic.php
@@ -0,0 +1,11 @@
+<?php
+print '108' + '15';
+print '108' + '1.5XYZ';
+// print '108' + 'XYZ10';
+print '108' + '1.5XYZ10';
+print '<br />';
+print '108' + '1.5E1';
+print '108' + '0b11';
+print '<br />';
+print '108' + '010';
+print '108' + '0x1A';
---
+++ chap03/increment.php
@@ -0,0 +1,10 @@
+<?php
+$x = 10;
+$y = ++$x;
+print $x;
+print $y;
+
+$i = 10;
+$j = $i++;
+print $i;
+print $j;
