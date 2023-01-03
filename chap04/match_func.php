f39412ac431e099f46bd5aeb9d96c2fa13919672
Shusuke Nakamura <shusuke.pana@gmail.com>
2023-01-03 17:32:06 +0900

    4.1 条件分岐の学習終了

---
+++ chap04/match_func.php
@@ -0,0 +1,23 @@
+<?php
+$data = 10;
+
+print match ($data) {
+    hoge() => 'hogeがマッチ',
+    foo() => 'fooがマッチ',
+    bar() => 'barがマッチ'
+};
+
+function hoge()
+{
+    return 1;
+}
+
+function foo()
+{
+    return 10;
+}
+
+function bar()
+{
+    return 100;
+}
---
+++ chap04/switch_ng.php
@@ -0,0 +1,10 @@
+<?php
+$exp = '3E2';
+switch ($exp) {
+    case 300:
+        print '値は300です。';
+        break;
+    case '3E2':
+        print '値は3E2です。';
+        break;
+}
